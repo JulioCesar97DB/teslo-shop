39b06bd9536ff385564460c4cb8680c795594c85
JCD <juliocesar97db@gmail.com>
2023-04-30 18:20:35 -0400

    temp main page, page 404 created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { ShopLayout } from "@/components/layouts";
-import { Typography } from "@mui/material";
+import { initialData } from "@/database/products";
+import { Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material";
 
 
 export default function Home() {
@@ -7,6 +8,24 @@ export default function Home() {
     <ShopLayout title={'Teslo - Home'} pageDescription={'Find the best products here'}>
       <Typography variant='h1' component='h1'>Store</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>All Products</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map( product => (
+            <Grid item xs={6} sm={4} key={ product.slug }>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={ `products/${ product.images[0] }`}
+                    alt={ product.title }
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }
